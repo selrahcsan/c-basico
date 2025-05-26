a42ceeff5d6337647a3bac35c97ffaa0d49b5df9
Charles Santana <charlesstna@protonmail.me>
2025-05-26 14:50:24 -0300

    Adicionado o exercicio 2

---
+++ aula-01/exercico2.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // O que é isso e para que serve? 
+
+// Por que todo o programa é escrito na função 'main()'?
+int main(void) {
+    
+    puts("Salve, simpatia!"); // O que faz e como se usa a função 'puts()'?
+
+    return 0; // Para que serve esta instrução? 
+}
