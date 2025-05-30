0eaa3937fec31714fb4ddef98672e9bdcf616e68
Charles Santana <charlesstna@proton.me>
2025-05-30 12:49:14 -0300

    Acrescentado o enunciado do exercício2

--- aulas/02/operacoes-aritimeticas.c
+++ aulas/02/operacoes-aritimeticas.c
@@ -1,3 +1,21 @@
+// Escreva um programa que, dado um valor inteiro associado à variável num,
+// calcule e imprima os resultados das seguintes operações:
+  
+// num multiplicado por 15;
+//  num somado a 42;
+  
+// 123 menos num;
+// Divisão de num por 5;
+// Resto da divisão de num por 5.
+
+// Esquema geral da saída do programa:
+// NUM x 15 = VALOR
+// NUM + 42 = VALOR
+// 123 - NUM = VALOR
+// NUM / 5 = VALOR
+// RESTO DE NUM / 5 = VALOR
+
+
 #include <stdio.h>
 
 int main(){
