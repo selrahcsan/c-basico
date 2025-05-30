7419afc9ad2d1006379712076a13abeaf5739cfb
Charles Santana <charlesstna@proton.me>
2025-05-30 12:38:40 -0300

    Revilvido o exercicio 2

---
+++ aulas/02/operacoes-aritimeticas.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	int num = 5;
+
+	printf("%d x 15 = %d\n", num, num * 15);
+	printf("%d + 42 = %d\n", num, num + 42);
+	printf("123 - %d = %d\n", num, 123 - num);
+	printf("%d / 5 =  %d\n", num, num / 5);
+	printf("Resto de %d / 5 = %d\n", num, num % num);
+
+
+}
+
+
+
+
+
